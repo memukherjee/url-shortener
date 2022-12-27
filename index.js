e4cb7397ffa54c651c9cd447132be52beaac5166
Akash Mukherjee <akash3.11.2000@gmail.com>
2022-12-27 20:43:31 +0530

    dbconnect function is called as async

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const connectDB = require('./config/dbConfig');
-
-
-// Connect to database
-connectDB();
+const connectDB = require("./config/dbConfig");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, async () => {
+  try {
+    await connectDB();
+    console.log(`Server is running on port ${process.env.PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
 });
